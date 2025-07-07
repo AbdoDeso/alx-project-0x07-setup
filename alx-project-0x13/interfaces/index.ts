4fef0a853835fa74793398c6ea9b105d40bcb7e0
Abdullah Eldesoky <microstar747@gmail.com>
2025-07-07 05:43:30 +0300

    Add files via upload

---
+++ alx-project-0x13/interfaces/index.ts
@@ -0,0 +1,28 @@
+
+import { ReactNode } from "react";
+
+export interface ReactComponentProps {
+  children: ReactNode
+}
+
+
+export interface GeneratedImageProps {
+  imageUrl: string
+  prompt: string
+  width?: string
+  height?: string
+  action: (imagePath: string) => void
+}
+
+export type RequestProps = {
+  prompt: string;
+}
+
+export type ImageProps = Pick<GeneratedImageProps, "imageUrl" | "prompt">
+
+// types.ts or interfaces.ts
+export interface PromptRequestBody {
+  prompt: string;
+
+}
+
